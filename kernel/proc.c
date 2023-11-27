de98266cc3d2582bd3277bcebdc3301021597782
zhu-to-u <2528487925@qq.com>
2023-11-27 19:13:03 +0800

    pgtbltest-pass

--- kernel/proc.c
+++ kernel/proc.c
@@ -127,6 +127,15 @@ found:
     return 0;
   }
 
+  // 分配 usyscall 页
+  //额外开辟空间给struct usyscall,并初始化填入pid数据支撑内存页
+  if((p->usyscall = (struct usyscall *)kalloc()) == 0){
+    freeproc(p);
+    release(&p->lock);
+    return 0;
+  }
+  p->usyscall->pid = p->pid;
+
   // An empty user page table.
   p->pagetable = proc_pagetable(p);
   if(p->pagetable == 0){
@@ -135,13 +144,6 @@ found:
     return 0;
   }
 
-    // 分配 usyscall 页
-  if((p->usyscall = (struct usyscall *)kalloc()) == 0){
-    freeproc(p->usyscall);
-    release(&p->lock);
-    return 0;
-  }
-  p->usyscall->pid = p->pid;
 
   // Set up new context to start executing at forkret,
   // which returns to user space.
@@ -207,9 +209,9 @@ proc_pagetable(struct proc *p)
     return 0;
   }
 
-  if(mappages(pagetable, USYSCALL, PGSIZE, (uint64)(p->usyscall), PTE_R | PTE_U) < 0){
-    uvmunmap(pagetable, TRAMPOLINE, 1, 0);
-    uvmunmap(pagetable, TRAPFRAME, 1, 0);
+  if(mappages(pagetable, USYSCALL, PGSIZE, 
+              (uint64)(p->usyscall), PTE_R | PTE_U) < 0){
+    uvmunmap(pagetable, USYSCALL, 1, 0);
     uvmfree(pagetable, 0);
     return 0;
   }
