b88512f43bf45275867c93c64e4b4cfa8b054733
zhu-to-u <2528487925@qq.com>
2023-12-09 21:08:50 +0800

    感觉这个lab不是很难主要就是一些加锁的逻辑

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -8,6 +8,8 @@
 #define NBUCKET 5
 #define NKEYS 100000
 
+pthread_mutex_t locks[NBUCKET];
+
 struct entry {
   int key;
   int value;
@@ -17,31 +19,38 @@ struct entry *table[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
 
-
+//提供时间戳
 double
 now()
 {
- struct timeval tv;
- gettimeofday(&tv, 0);
- return tv.tv_sec + tv.tv_usec / 1000000.0;
+ struct timeval tv;//存储时间戳
+ gettimeofday(&tv, 0);//获取当前时间戳
+ return tv.tv_sec + tv.tv_usec / 1000000.0;//返回当前时间秒数
 }
 
+//在链表中插入新的节点  头插
 static void 
 insert(int key, int value, struct entry **p, struct entry *n)
 {
+  //使用malloc分配内存以存储新的entry节点
   struct entry *e = malloc(sizeof(struct entry));
+  //设置新的节点的键值对和下一个节点的指针
   e->key = key;
   e->value = value;
   e->next = n;
+
+  //更新传入的指针，使其指向新插入的节点
   *p = e;
 }
 
+//在哈希中插入或更新键值对
 static 
 void put(int key, int value)
 {
   int i = key % NBUCKET;
 
-  // is the key already present?
+  // is the key already present?判断键是否已经存在于哈希桶中
+  pthread_mutex_lock(&locks[i]);
   struct entry *e = 0;
   for (e = table[i]; e != 0; e = e->next) {
     if (e->key == key)
@@ -54,23 +63,26 @@ void put(int key, int value)
     // the new is new.
     insert(key, value, &table[i], table[i]);
   }
-
+  pthread_mutex_unlock(&locks[i]);
 }
-
+//查找特定键对应的节点
 static struct entry*
 get(int key)
 {
   int i = key % NBUCKET;
 
-
+  //pthread_mutex_lock(&locks[i]);
   struct entry *e = 0;
   for (e = table[i]; e != 0; e = e->next) {
     if (e->key == key) break;
   }
 
+  //pthread_mutex_unlock(&locks[i]);
+
   return e;
 }
 
+//线程函数 用于在哈希表中插入键值对
 static void *
 put_thread(void *xa)
 {
@@ -84,6 +96,7 @@ put_thread(void *xa)
   return NULL;
 }
 
+//线程函数用于在哈希表中查找特定键对应的节点
 static void *
 get_thread(void *xa)
 {
@@ -114,6 +127,14 @@ main(int argc, char *argv[])
   tha = malloc(sizeof(pthread_t) * nthread);
   srandom(0);
   assert(NKEYS % nthread == 0);
+
+  //加上锁的初始化就会段错误  but why？？
+  //因为i小于的宏定义给错了
+  for(int i=0;i<NBUCKET;i++)
+  {
+    pthread_mutex_init(&locks[i],NULL);
+  }
+
   for (int i = 0; i < NKEYS; i++) {
     keys[i] = random();
   }
--- user/uthread.c
+++ user/uthread.c
@@ -7,14 +7,36 @@
 #define RUNNING     0x1
 #define RUNNABLE    0x2
 
-#define STACK_SIZE  8192
-#define MAX_THREAD  4
+#define STACK_SIZE  8192 //栈的大小？
+#define MAX_THREAD  4  //最大线程数？
 
+//参考kernel/proc.h struct cpu
+struct thread_context{
+  uint64 ra;
+  uint64 sp;
 
+  uint64 s0;
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
+
+};
+
+//每个用户空间都有自己的栈空间、状态、上下文信息
 struct thread {
   char       stack[STACK_SIZE]; /* the thread's stack */
-  int        state;             /* FREE, RUNNING, RUNNABLE */
+  int        state;             /*状态 FREE, RUNNING, RUNNABLE */
+  struct  thread_context context;//保存恢复寄存器的上下文
 };
+
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
 extern void thread_switch(uint64, uint64);
@@ -46,7 +68,7 @@ thread_schedule(void)
       next_thread = t;
       break;
     }
-    t = t + 1;
+    t = t + 1;//移动到下一个可运行的线程
   }
 
   if (next_thread == 0) {
@@ -54,7 +76,7 @@ thread_schedule(void)
     exit(-1);
   }
 
-  if (current_thread != next_thread) {         /* switch threads?  */
+  if (current_thread != next_thread) {         /* 切换线程 switch threads?  */
     next_thread->state = RUNNING;
     t = current_thread;
     current_thread = next_thread;
@@ -62,6 +84,7 @@ thread_schedule(void)
      * Invoke thread_switch to switch from t to next_thread:
      * thread_switch(??, ??);
      */
+    thread_switch((uint64)&t->context, (uint64)&next_thread->context);//上下文切换
   } else
     next_thread = 0;
 }
@@ -76,6 +99,8 @@ thread_create(void (*func)())
   }
   t->state = RUNNABLE;
   // YOUR CODE HERE
+  t->context.ra = (uint64)func;
+  t->context.sp = (uint64)t->stack + STACK_SIZE - 8;
 }
 
 void 
@@ -85,26 +110,29 @@ thread_yield(void)
   thread_schedule();
 }
 
+//关键字作用：禁止编译器优化|多线程同步（标记共享变量）
 volatile int a_started, b_started, c_started;
 volatile int a_n, b_n, c_n;
 
+//线程函数的实现
 void 
 thread_a(void)
 {
   int i;
   printf("thread_a started\n");
   a_started = 1;
-  while(b_started == 0 || c_started == 0)
+  while(b_started == 0 || c_started == 0)//这是一个循环，它会持续运行
+  //直到变量 b_started 和 c_started 都变为非零（即其他线程已经启动）为止，是线程同步的一种方式
     thread_yield();
   
   for (i = 0; i < 100; i++) {
     printf("thread_a %d\n", i);
     a_n += 1;
-    thread_yield();
+    thread_yield();//允许线程主动让出cpu执行权
   }
   printf("thread_a: exit after %d\n", a_n);
 
-  current_thread->state = FREE;
+  current_thread->state = FREE;//线程a已经执行完成
   thread_schedule();
 }
 
